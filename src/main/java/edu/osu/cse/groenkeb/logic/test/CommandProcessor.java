2761a090a988a0729a5d2d77770dd3230520ec7f
Brian Groenke <groenke.5@buckeyemail.osu.edu>
2017-03-07 00:03:29 -0500

    Fix command type bound.

--- src/main/java/edu/osu/cse/groenkeb/logic/test/CommandProcessor.java
+++ src/main/java/edu/osu/cse/groenkeb/logic/test/CommandProcessor.java
@@ -2,10 +2,10 @@ package edu.osu.cse.groenkeb.logic.test;
 
 public interface CommandProcessor<T extends Context>
 {
-  public interface Command<T>
+  public interface Command<T extends Context>
   {
     /**
-     * Executes the command with the current and context and returns the result.
+     * Executes the command with the current context and returns the result.
      * @param current
      * @return the new context after execution
      */
