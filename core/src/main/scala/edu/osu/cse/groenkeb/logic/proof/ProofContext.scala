6ecfb6524bb85f251a703a6a13c2cdf2d1c0a3d1
Brian Groenke <groenke.5@buckeyemail.osu.edu>
2017-10-30 12:18:12 -0400

    Remove discharge set from ProofContext.

--- core/src/main/scala/edu/osu/cse/groenkeb/logic/proof/ProofContext.scala
+++ core/src/main/scala/edu/osu/cse/groenkeb/logic/proof/ProofContext.scala
@@ -9,39 +9,19 @@ object ProofContext {
             premises: Seq[Premise]) = new ProofContext(goal, rules, premises.toSet)
   
   def apply(goal: Sentence, premises: Seq[Premise] = Nil)(implicit rules: RuleSet) = new ProofContext(goal, rules, premises.toSet)
-  
-  private def apply(goal: Sentence,
-                    rules: RuleSet,
-                    premises: Seq[Premise],
-                    discharges: Seq[Assumption]) = new ProofContext(goal, rules, premises.toSet, discharges)
 }
 
-case class ProofContext private (val goal: Sentence,
+case class ProofContext (val goal: Sentence,
                                  val rules: RuleSet,
-                                 val premises: Set[Premise],
-                                 val discharges: Seq[Assumption]) {
-  def this(goal: Sentence, rules: RuleSet, premises: Set[Premise]) = this(goal, rules, premises, Nil)
+                                 val premises: Set[Premise]) {
   
-  def withAssumptions(newAssumptions: Assumption*) = ProofContext(goal, rules, premises ++ newAssumptions, discharges)
+  def withAssumptions(newAssumptions: Assumption*) = ProofContext(goal, rules, premises ++ newAssumptions)
   
-  def withRuleSet(newRules: RuleSet) = ProofContext(goal, newRules, premises, discharges)
+  def withRuleSet(newRules: RuleSet) = ProofContext(goal, newRules, premises)
   
-  def withGoal(newGoal: Sentence) = ProofContext(newGoal, rules, premises, discharges)
+  def withGoal(newGoal: Sentence) = ProofContext(newGoal, rules, premises)
   
-  def withDischarged(assumptions: Assumption*) = {
-    require(assumptions.toSet subsetOf premises)
-    ProofContext(goal,
-                 rules,
-                 premises filterNot { assumptions.toSet },
-                 discharges ++ assumptions)           
-  }
-                                                               
-  def withDischarge(discharged: Assumption) = {
-    require(premises.contains(discharged))
-    ProofContext(goal, rules, premises, discharges :+ discharged)
-  }
-  
-  def lessPremises(restrict: Premise*) = ProofContext(goal, rules, premises.filterNot { p => restrict.exists { r => r.matches(p) } }, discharges)
+  def lessPremises(restrict: Premise*) = ProofContext(goal, rules, premises.filterNot { p => restrict.exists { r => r.matches(p) } })
   
   def has(premise: Premise) = premises.exists { p => p.matches(premise) }
   
