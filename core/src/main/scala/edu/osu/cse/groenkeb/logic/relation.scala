8aedbb51c8a953437b0bf875efe79c70cc3f87af
Brian Groenke <bgroe8@gmail.com>
2018-03-24 01:54:13 -0400

    Fix relation type breaking auto-json

--- core/src/main/scala/edu/osu/cse/groenkeb/logic/relation.scala
+++ core/src/main/scala/edu/osu/cse/groenkeb/logic/relation.scala
@@ -3,8 +3,8 @@ package edu.osu.cse.groenkeb.logic
 /**
  * Relational ordered pair (s1, s2)
  */
-abstract class Relation(val predicate: Predicate, val rank: Int)
-case class ObjectRelation(private val p: Predicate, val terms: Term*) extends Relation(p, terms.length) {
+sealed abstract class Relation(val predicate: Predicate, val rank: Int)
+case class ObjectRelation(p: Predicate, terms: Term*) extends Relation(p, terms.length) {
   override def toString = String.format("%s[%s]", predicate, terms.mkString(","))
 }
-case class EmptyRelation(private val p: Predicate, private val r: Int) extends Relation(p, r)
+case class EmptyRelation(p: Predicate, r: Int) extends Relation(p, r)
