f53f7cefc66e905bac2cf04e71f5fda713542a00
Brian Groenke <bgroe8@gmail.com>
2018-04-09 22:46:12 -0400

    Remove unnecessary types

--- learn/src/main/scala/edu/osu/cse/groenkeb/logic/proof/engine/learn/q/QModel.scala
+++ learn/src/main/scala/edu/osu/cse/groenkeb/logic/proof/engine/learn/q/QModel.scala
@@ -4,6 +4,9 @@ import edu.osu.cse.groenkeb.logic.proof.engine.ProofStrategy.Action
 import edu.osu.cse.groenkeb.logic.proof.engine.learn.ProblemState
 
 trait QModel {
+  // Sort by negative Q-value, thus ensuring descending order
+  implicit val orderByHighestQValue = Ordering[Double].on((qv: QValue) => -qv.value)
+  
   def computeQValues(state: ProblemState, actions: Seq[Action]): Seq[QValue]
   def update(params: QUpdate, availableActions: Seq[Action])
 }
